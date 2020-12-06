f562623a2c78228cdeac1178ed9b9a7f2ced4539
pawmandh <mandhanpawan7@gmail.com>
2020-12-07 01:38:16 +0530

    get product status api added
    product status api added CartController
    CartService interface added
    CartServiceImpl class added
    ProductStatusDTO added

--- src/main/java/com/cart/controller/CartController.java
+++ src/main/java/com/cart/controller/CartController.java
@@ -1,9 +1,23 @@
 package com.cart.controller;
 
+import com.cart.dto.ProductStatusDTO;
+import com.cart.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cart")
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping("/product-status/{productId}")
+    public ProductStatusDTO productStatus(@PathVariable Long productId) {
+        return cartService.productStatus(productId);
+    }
 
 }
