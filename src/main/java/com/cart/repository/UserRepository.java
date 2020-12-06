a902456a6c12d3772f58329be0a99c833ace5d38
pawmandh <mandhanpawan7@gmail.com>
2020-12-07 04:19:57 +0530

    required models added
    Cart, Product, ProductCategory, User

---
+++ src/main/java/com/cart/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cart.repository;
+
+
+import com.cart.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
