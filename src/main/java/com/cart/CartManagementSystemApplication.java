98514eaf4c450ca7204cf8b583965b886a5254dc
Pawan Mandhan <pawmandh@publicisgroupe.net>
2020-12-06 22:33:16 +0530

    initial commit

---
+++ src/main/java/com/cart/CartManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartManagementSystemApplication.class, args);
+	}
+
+}
