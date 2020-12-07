7d3771dafad6d0e49ab52ba52fc3656ef14afd91
pawmandh <mandhanpawan7@gmail.com>
2020-12-07 18:00:01 +0530

    getCartByStatus API added

--- src/main/java/com/cart/controller/CartController.java
+++ src/main/java/com/cart/controller/CartController.java
@@ -1,7 +1,9 @@
 package com.cart.controller;
 
+import com.cart.dto.CartDTO;
 import com.cart.dto.ProductStatusDTO;
 import com.cart.service.CartService;
+import com.cart.util.CartStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,11 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cart")
+@RequestMapping("/carts")
 public class CartController {
 
     @Autowired
     private CartService cartService;
+
+    // API [1]
+    @GetMapping("/{status}")
+    public CartDTO getCartsByStatus(@PathVariable CartStatus status) {
+        return null;
+    }
 
     // API [5]
     @GetMapping("/product-status/{productId}")
