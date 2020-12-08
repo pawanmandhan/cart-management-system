eb8d5ad0fc6c893e433592805471fd8113c3c63d
pawmandh <mandhanpawan7@gmail.com>
2020-12-08 15:45:22 +0530

    Invalid cart status(path variable) handled

--- src/main/java/com/cart/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/cart/exception/RestResponseEntityExceptionHandler.java
@@ -1,38 +1,32 @@
 package com.cart.exception;
 
-import org.springframework.core.convert.ConversionFailedException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
-    public @ResponseBody String handleConflict(IllegalArgumentException ex) {
-        return "This should be application specific";
-    }
+//    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+//    public @ResponseBody String handleConflict(IllegalArgumentException ex) {
+//        return "This should be application specific";
+//    }
+//
+//    @ExceptionHandler(value = {ConversionFailedException.class})
+//    public @ResponseBody String handleConflict2(ConversionFailedException ex) {
+//        return "This should be application specific 2";
+//    }
+//
+//    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+//    public @ResponseBody String handleConflict3(MethodArgumentNotValidException ex) {
+//        return "This should be application specific 3";
+//    }
 
-    @ExceptionHandler(value = {ConversionFailedException.class})
-    public @ResponseBody String handleConflict2(ConversionFailedException ex) {
-        return "This should be application specific 2";
-    }
-
-    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
-    public @ResponseBody String handleConflict3(MethodArgumentNotValidException ex) {
-        return "This should be application specific 3";
-    }
-
+    // handler on cart status invalid
     @ExceptionHandler(value = {MethodArgumentTypeMismatchException.class})
-    public @ResponseBody String handleConflict4(MethodArgumentTypeMismatchException ex) {
-        return "This should be application specific 4";
+    public @ResponseBody String handleInvalidCartStatus(MethodArgumentTypeMismatchException ex) {
+        return "Invalid Status: Please provide a valid status in the path variable";
     }
 
 }
